112964887f229ceec2b1adca590641e9aeee86f5
Enick Jhony <enick.jhony@gmail.com>
2023-05-17 12:20:06 -0400

    Rota para buscar todas as memórias

--- server/src/routes/memories.ts
+++ server/src/routes/memories.ts
@@ -3,9 +3,35 @@ import { FastifyInstance } from 'fastify'
 import { prisma } from '../lib/prisma'
 
 export async function memoriesRoutes(app: FastifyInstance) {
-  app.get('/users', async () => {
-    const users = await prisma.user.findMany()
+  app.get('/memories', async () => {
+    const memories = await prisma.memory.findMany({
+      orderBy:{
+        createdAt: 'asc',
+      }
+    })
+
+    return memories.map(memory => {
+      return {
+        id: memory.id,
+        coverUrl: memory.coverUrl,
+        except: memory.content.substring(0, 115).concat('...'),
+      }
+    })
+  })
+
+  app.get('/memories:id', async () => {
     
-    return users
+  })
+
+  app.post('/memories', async () => {
+    
+  })
+
+  app.put('/memories:id', async () => {
+    
+  })
+
+  app.delete('/memories:id', async () => {
+    
   })
 }
