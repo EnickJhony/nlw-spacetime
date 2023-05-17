98295324bc7035dede249e63e5ee5aa3f912279d
Enick Jhony <enick.jhony@gmail.com>
2023-05-17 12:11:56 -0400

    Criando arquivos de rotas e modificando no projeto

---
+++ server/src/routes/memories.ts
@@ -0,0 +1,11 @@
+/* eslint-disable prettier/prettier */
+import { FastifyInstance } from 'fastify'
+import { prisma } from '../lib/prisma'
+
+export async function memoriesRoutes(app: FastifyInstance) {
+  app.get('/users', async () => {
+    const users = await prisma.user.findMany()
+    
+    return users
+  })
+}
