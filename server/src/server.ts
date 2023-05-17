de1a61471da200dd5baf1a08ef245ffd49f88e54
Enick Jhony <enick.jhony@gmail.com>
2023-05-17 12:49:14 -0400

    Adicionando e configurando Cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,13 @@
 /* eslint-disable prettier/prettier */
 import fastify from 'fastify'
+import cors from "@fastify/cors"
 import { memoriesRoutes } from './routes/memories'
 
 const app = fastify()
+
+app.register(cors, {
+  origin: true,
+})
 
 app.register(memoriesRoutes)
 
